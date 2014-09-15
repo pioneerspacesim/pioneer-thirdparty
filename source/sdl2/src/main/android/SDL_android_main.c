9b0f7bf4a7e89eee30f6b13f8115fafffece06cd
John Bartholomew <jpa.bartholomew@gmail.com>
2014-09-15 23:38:36 +0100

    Update SDL2 source to version 2.0.3

--- source/sdl2/src/main/android/SDL_android_main.c
+++ source/sdl2/src/main/android/SDL_android_main.c
@@ -1,5 +1,7 @@
-
-#include "SDL_config.h"
+/*
+    SDL_android_main.c, placed in the public domain by Sam Lantinga  3/13/14
+*/
+#include "../../SDL_internal.h"
 
 #ifdef __ANDROID__
 
@@ -11,10 +13,10 @@
 *******************************************************************************/
 #include <jni.h>
 
-// Called before SDL_main() to initialize JNI bindings in SDL library
+/* Called before SDL_main() to initialize JNI bindings in SDL library */
 extern void SDL_Android_Init(JNIEnv* env, jclass cls);
 
-// Start up the SDL app
+/* Start up the SDL app */
 void Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject obj)
 {
     /* This interface could expand with ABI negotiation, calbacks, etc. */
@@ -30,7 +32,7 @@ void Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject
     status = SDL_main(1, argv);
 
     /* Do not issue an exit or the whole application will terminate instead of just the SDL thread */
-    //exit(status);
+    /* exit(status); */
 }
 
 #endif /* __ANDROID__ */
